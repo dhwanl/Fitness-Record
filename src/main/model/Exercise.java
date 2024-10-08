aaae710cd052fc8abe4262eb935d17151fa18843
dhwanyi <dhwanyi@student.ubc.ca>
2024-10-07 21:05:20 -0700

    Added specification of Exercise class

--- src/main/model/Exercise.java
+++ src/main/model/Exercise.java
@@ -1,6 +1,5 @@
 package model;
 
-<<<<<<< HEAD
 // Specify the exercise type, weight lifted, and number of sets and reps, as well as users can add and remove exercises
 public class Exercise {
     private String exerciseName;    // the type of exercise
@@ -8,20 +7,6 @@ public class Exercise {
     private int numSets;            // track the number of sets of the exercise
     private int numReps;            // track the number of repetition of the exercise
     private String muscleType;      // the type of muscle users use with exercise
-=======
-import java.util.ArrayList;
-import java.util.List;
-
-// Specify the exercise type, weight lifted, and number of sets and reps, as well as users can add and remove exercises
-public class Exercise {
-    private static List<Exercise> exerciseList = new ArrayList<Exercise>(); // store all exercises users added
-    
-    private String exerciseType;    // the type of exercise
-    private int weightLifted;       // track the weight lifted in killogram
-    private int numSets;            // track the number of sets of the exercise
-    private int numReps;            // track the number of repetition of the exercise
-
->>>>>>> 785c1e7d7e4858cd445dde5048762c1101942a3f
     /*
      * REQUIRES: exerciseType has a non-zero length
      * EFFECTS: name of the type of exercise is set to exerciseType, and check if this exerciseType is already in exerciseList; 
@@ -32,7 +17,6 @@ public class Exercise {
     }
 
     /*
-<<<<<<< HEAD
      * REQUIRES: exerciseType has a non-zero length; weightLifted >= 0.0; numSets and numReps >= 0
      * EFFECTS: name of the type of exercise is set to exerciseType and check if this exerciseType is already in exerciseList; 
      *          weightLifted, numSets, and numReps are set to the given value
@@ -42,22 +26,6 @@ public class Exercise {
     }
 
     public String getExerciseName() {
-=======
-     * REQUIRES: exerciseType has a non-zero length; weightLifted, numSets, and numReps have non-zero integer
-     * EFFECTS: name of the type of exercise is set to exerciseType and check if this exerciseType is already in exerciseList; 
-     *          weightLifted, numSets, and numReps are set to the given value
-     */
-    public Exercise(String exerciseType, int weightLifted, int numSets, int numReps) {
-        // stub
-    }
-
-    public static List<Exercise> getExerciseList() {
-        // stub
-        return null;
-    }
-
-    public String getExerciseType() {
->>>>>>> 785c1e7d7e4858cd445dde5048762c1101942a3f
         // stub
         return "";
     }
@@ -67,7 +35,6 @@ public class Exercise {
         return 0;
     }
 
-<<<<<<< HEAD
     public int getNumSets() {
         // stub
         return 0;
@@ -98,49 +65,5 @@ public class Exercise {
     public void setMuscleType(int muscleType) {
         // stub
     }
-=======
-    public int getNumReps() {
-        //stub
-        return 0;
-    }
-
-    public void setWeightLifted() {
-        // stub
-    }
-
-    public void setNumReps() {
-        // stub
-    }
-
-    /*
-     * EFFECTS: return true if there is the same type of exercise, otherwise false
-     */
-    public boolean isExerciseInList() {
-        //stub
-        return false;
-    }
-
-    /*
-     * REQUIRES: exerciseType has a non-zero length
-     * MODIFIES: this
-     * EFFECTS: add the name of the type of exercise if it is not in exerciseList. Otherwise, print a user has added it already
-     */
-    public void add(String exerciseType) {
-        // stub
-    }
-
-    /*
-     * REQUIRES: exerciseType has a non-zero length
-     * MODIFIES: this
-     * EFFECTS: remove the name of the type of exercise if it is in exerciseList. Otherwise, print "there is no this type of exercise you would like to remove"
-     */
-    public void remove(String exerciseType) {
-        // stub
-    }
-
-
-    
-
->>>>>>> 785c1e7d7e4858cd445dde5048762c1101942a3f
 
 }
