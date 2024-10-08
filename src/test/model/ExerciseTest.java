5a75d060caf3b931cae05d99333f7a625c3a13cb
dhwanyi <dhwanyi@student.ubc.ca>
2024-10-07 22:59:48 -0700

    Added tests for Exercise class

--- src/test/model/ExerciseTest.java
+++ src/test/model/ExerciseTest.java
@@ -2,21 +2,33 @@ package model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ExerciseTest {
     private Exercise exercise;
     
+    @BeforeEach
+    void beforeTest() {
+        exercise = new Exercise("Leg extension", Muscles.LEGS);
+    }
+
     @Test
     void testConstrutor() {
-        exercise = new Exercise("Leg extension");
         assertEquals("Leg extension", exercise.getExerciseName());
+        assertEquals("LEGS", exercise.getMuscleType().toString());
 
-        exercise = new Exercise("Incline bench press", 60, 4, 10);
+        exercise = new Exercise("Incline bench press", Muscles.LEGS, 60, 4, 10);
         assertEquals("Incline bench press", exercise.getExerciseName());
+        assertEquals(Muscles.LEGS, exercise.getMuscleType());
         assertEquals(60, exercise.getWeightLifted());
         assertEquals(4, exercise.getNumSets());
         assertEquals(10, exercise.getNumReps());
+    }
+
+    @Test
+    void testIsLengthZero() {
+        assertFalse(exercise.isLengthZero(exercise.getExerciseName()));
     }
 
 }
