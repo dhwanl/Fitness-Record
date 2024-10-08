eec2e3a19e8db03deaefb89bbb8c6aaeebc2d8b7
dhwanyi <dhwanyi@student.ubc.ca>
2024-10-07 23:01:54 -0700

    Added implementation for Exercise class

--- src/main/model/Exercise.java
+++ src/main/model/Exercise.java
@@ -1,19 +1,27 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // Specify the exercise type, weight lifted, and number of sets and reps, as well as users can add and remove exercises
 public class Exercise {
     private String exerciseName;    // the type of exercise
     private double weightLifted;    // track the weight lifted in killogram
     private int numSets;            // track the number of sets of the exercise
     private int numReps;            // track the number of repetition of the exercise
-    private String muscleType;      // the type of muscle users use with exercise
+    private Muscles muscleType;     // the type of muscle users use with exercise
+
     /*
-     * REQUIRES: exerciseType has a non-zero length
+     * REQUIRES: exerciseName has a non-zero length
      * EFFECTS: name of the type of exercise is set to exerciseType, and check if this exerciseType is already in exerciseList; 
-     *          weightLifted, numSets, and numReps are initially set to zero
+     *          weightLifted, numSets, and numReps are initially set to zero; group exercises based on muscleType;
      */
-    public Exercise(String exerciseType) {
-        // stub
+    public Exercise(String exerciseName, Muscles muscleType) {
+        this.exerciseName = exerciseName.substring(0, 1).toUpperCase() + exerciseName.substring(1).toLowerCase();
+        this.muscleType = muscleType;
+        this.weightLifted = 0.0;
+        this.numSets = 0;
+        this.numReps = 0;
     }
 
     /*
@@ -21,49 +29,62 @@ public class Exercise {
      * EFFECTS: name of the type of exercise is set to exerciseType and check if this exerciseType is already in exerciseList; 
      *          weightLifted, numSets, and numReps are set to the given value
      */
-    public Exercise(String exerciseType, double weightLifted, int numSets, int numReps) {
-        // stub
+    public Exercise(String exerciseName, Muscles muscleType, double weightLifted, int numSets, int numReps) {
+        this.exerciseName = exerciseName.substring(0, 1).toUpperCase() + exerciseName.substring(1).toLowerCase();
+        this.muscleType = muscleType;
+        this.weightLifted = weightLifted;
+        this.numSets = numSets;
+        this.numReps = numReps;
     }
 
+    /*
+     * EFFECTS: return false if the length of the name of exercise is not zero.
+     *          Otherwise, return true with the error message
+     */
+    public boolean isLengthZero(String exerciseName) {
+        if(exerciseName.length() > 0){
+            return false;
+        }
+
+        return true;
+    }
+
+    // getters
     public String getExerciseName() {
-        // stub
-        return "";
+        return this.exerciseName;
     }
 
-    public int getWeightLifted() {
-        // stub
-        return 0;
+    public double getWeightLifted() {
+        return this.weightLifted;
     }
 
     public int getNumSets() {
-        // stub
-        return 0;
+        return this.numSets;
     }
 
     public int getNumReps() {
-        // stub
-        return 0;
+        return this.numReps;
     }
 
-    public String getMuscleType() {
-        // stub
-        return null;
+    public Muscles getMuscleType() {
+        return this.muscleType;
     }
     
+    // setters
     public void setExerciseName(String exerciseName) {
-        // stub
+        this.exerciseName = exerciseName;
     }
 
     public void setWeightLifted(int weight) {
-        // stub
+        this.weightLifted = weight;
     }
 
     public void setNumReps(int reps) {
-        // stub
+        this.numReps = reps;
     }
 
-    public void setMuscleType(int muscleType) {
-        // stub
+    public void setMuscleType(Muscles muscleType) {
+        this.muscleType = muscleType;
     }
 
 }
