0b5ea383fe4ad5106ba84fc2060cee6d2674d41b
dhwanyi <dhwanyi@student.ubc.ca>
2024-10-10 00:51:15 -0700

    Update specification and implementation for Log class

--- src/main/model/Log.java
+++ src/main/model/Log.java
@@ -4,15 +4,33 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+ * Stores information about exercises and the date performed by users, as well as the exercise details.
+ * 
+ * In addition, all logs are stored in a list, allowing Log class to provide functional management for exercise logs.
+ * i.e., filtering logs by muscle types or dates, 
+ *       finding a specific log by exercise name and date.
+ *       updating exercise details
+ */
 public class Log {
-    public static List<Log> exercises = new ArrayList<Log>();           // store all exercises users did
+    private static List<Log> exercises = new ArrayList<Log>();  // store all exercises users did
 
-    private Exercise exercise;
-    private String date;                                                 // date in the format "year/month/date"
+    private Exercise exercise;     // data type Exercise variable
+    private String date;           // date in the format "year/month/date"
 
+    
     /*
-     * EFFECTS: Data type Exercise is set to exercse in log class
-     *          and String date (year/month/date) is set to date in log class
+     * EFFECTS: create an empty Log instance with no exercise or date
+     */
+    public Log() {
+        this.exercise = null;
+        this.date = null;
+    }
+    
+    /*
+     * REQUIRES: date must follow the format (yyyy/mm/dd)
+     * EFFECTS: Initialize the Log instance with the provided Exercise object
+     *          and set the date
      */
     public Log(Exercise exercise, String date) {
         this.exercise = exercise;
@@ -20,15 +38,17 @@ public class Log {
     }
 
     /*
-     * REQUIRES: date must be in the format (year/month/date); The length of exerciseName is not zero
+     * REQUIRES: date must be in the format (year/month/date); 
+     *           the length of exerciseName is not zero
      * EFFECTS: return index if this finds the matched one in the list using the name of exercise and date
      */
     public int findMatchedLog(String exerciseName, String date) {
-    for(int i = 0; i < exercises.size(); i++) {
-        String tempName = exercises.get(i).getExercise().getExerciseName();
-        String tempDate = exercises.get(i).getDate();
 
-            if(tempName.equals(exerciseName) && tempDate.equals(date)) {
+        for (int i = 0; i < exercises.size(); i++) {
+            String tempName = exercises.get(i).getExercise().getExerciseName();
+            String tempDate = exercises.get(i).getDate();
+
+            if (tempName.equalsIgnoreCase(exerciseName) && tempDate.equals(date)) {
                 return i;
             }
         }
@@ -37,39 +57,133 @@ public class Log {
     }
 
     /*
-     * EFFECTS: filter exercises by muscle type
-     *          i.e., CHEST, BACK, SHOULDERS etc.
+     * REQUIRES: newWeight >= 0
+     * MODIFIES: this
+     * EFFECTS: update weight lifted
      */
-    public List<Log> filteredExercises(Muscles type) {
-        List<Log> filteredLogs = new ArrayList<Log>();
+    public void updateWeight(double newWeight) {
+        this.getExercise().setWeightLifted(newWeight);
+    }
 
-        for(int i = 0; i < exercises.size(); i++) {
-            if(exercises.get(i).getExercise().getMuscleType() == type) {
-                filteredLogs.add(exercises.get(i));
-            }
-        }
-
-        return filteredLogs;
+    /*
+     * REQUIRES: the length of newExerciseName must be greater than zero
+     * MODIFIES: this
+     * EFFECTS: update exercise name
+     */
+    public void updateName(String newExerciseName) {
+        this.getExercise().setExerciseName(newExerciseName);
     }
 
     /*
      * MODIFIES: this
-     * EFFECTS: add exercises users did into log list
+     * EFFECTS: update muscle type
+     */
+    public void updateMuscleType(Muscles newType) {
+        this.getExercise().setMuscleType(newType);
+    }
+
+    /*
+     * REQUIRES: newSets >= 0
+     * MODIFIES: this
+     * EFFECTS: update the number of set
+     */
+    public void updateSets(int newSets) {
+        this.getExercise().setNumSets(newSets);
+    }
+
+    /*
+     * REQUIRES: newReps >= 0
+     * MODIFIES: this
+     * EFFECTS: update the number of repetition
+     */
+    public void updateReps(int newReps) {
+        this.getExercise().setNumReps(newReps);
+    }
+
+    /*
+     * REQUIRES: response == 1 || response == 2;
+     *           date must be in the format (yyyy/mm/dd)
+     * EFFECTS: when response = 1, create a new list filtered by a specific muscle type
+     *          otherwise, create a new list filtered by a specific date.
+     *          return the new filtered list
+     */
+    public List<Log> filteredLog(int response, Muscles type, String date) {
+        List<Log> filteredLog = new ArrayList<Log>();
+
+        if (response == 1) {
+            filteredLog = new Log().filteredExercisesByType(type);
+        }
+
+        if (response == 2) {
+            filteredLog = new Log().filteredExercisesByDate(date);
+        }
+
+        return filteredLog;
+    }
+
+    /*
+     * EFFECTS: filter exercises by a specific muscle type
+     *          i.e., CHEST, BACK, SHOULDERS etc.
+     *          then, return a filtered list
+     */
+    public List<Log> filteredExercisesByType(Muscles type) {
+        List<Log> filteredByTypeLogs = new ArrayList<Log>();
+
+        for (int i = 0; i < exercises.size(); i++) {
+            if (exercises.get(i).getExercise().getMuscleType() == type) {
+                filteredByTypeLogs.add(exercises.get(i));
+            }
+        }
+
+        return filteredByTypeLogs;
+    }
+
+    /* 
+     * REQUIRES: the length of date > 0 and in the format (year/month/date)
+     * EFFECTS: filter exercises by date.
+     *          then, return a filtered list
+     *          
+     */
+    public List<Log> filteredExercisesByDate(String date) {
+        List<Log> filteredByDateLogs = new ArrayList<Log>();
+
+        for (int i = 0; i < exercises.size(); i++) {
+            if (exercises.get(i).getDate().equals(date)) {
+                filteredByDateLogs.add(exercises.get(i));
+            }
+        }
+
+        return filteredByDateLogs;
+    }
+
+    /*
+     * MODIFIES: this
+     * EFFECTS: add the registered exercise into the exercises list
      */
     public void addLogToExercisesList() {
         exercises.add(this);
     }
 
     /*
-     * REQUIRES: exerciseName has a non-zero length
+     * REQUIRES: exerciseName has a non-zero length;
+     *           date must be in the format (yyyy/mm/dd)
      * MODIFIES: this
-     * EFFECTS: remove the log by name of the type of exercise and date       
+     * EFFECTS: remove the log that matches the given exercise name and date from the list.
+     *          then, return the removed log if a match is found, otherwise return null       
      */
-    public void removeLogExercises(String exerciseName, String date) {
+    public Log removeLogExercises(String exerciseName, String date) {
         int matchedIndex = findMatchedLog(exerciseName, date);
-        exercises.remove(matchedIndex);
+
+        if (matchedIndex != -1) {
+            Log log = exercises.get(matchedIndex);
+            exercises.remove(matchedIndex);
+            return log;
+        }
+
+        return null;
     }
 
+    // getters
     public List<Log> getAllExercisesLog() {
         return exercises;
     }
