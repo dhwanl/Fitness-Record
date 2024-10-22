4b05e75d9dc6e00b7f3aa178df0d684f7b02ab93
dhwanyi <dhwanyi@student.ubc.ca>
2024-10-22 14:14:51 -0700

    Added test for Json

---
+++ src/test/persistence/JsonTest.java
@@ -0,0 +1,21 @@
+package persistence;
+
+import static org.junit.Assert.assertEquals;
+
+import model.Exercise;
+import model.Log;
+import model.Muscles;
+
+// Referenced from JsonSerialization Demo
+// https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
+public class JsonTest {
+    protected void checkExercise(Log log, String exerciseName, Muscles muscleType, double weightLifted, int numSets, int numReps) {
+        Exercise temp = log.getExercise();
+        
+        assertEquals(temp.getExerciseName(), exerciseName);
+        assertEquals(temp.getMuscleType(), muscleType);
+        assertEquals(temp.getWeightLifted(), weightLifted);
+        assertEquals(temp.getNumSets(), numSets);
+        assertEquals(temp.getNumReps(), numReps);
+    }
+}
