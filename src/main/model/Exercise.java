b1e18fa9ee91875e99ed8eec8ab49fe0b9254e86
Donghwan Lee <dhwanyi@gmail.com>
2025-10-30 17:02:01 -0700

    Delete unnecessary lines in the code

--- src/main/model/Exercise.java
+++ src/main/model/Exercise.java
@@ -1,7 +1,6 @@
 package model;
 
 import org.json.JSONObject;
-
 import persistence.Writable;
 
 // Specify the exercise type, weight lifted, and number of sets and reps, as well as users can add and remove exercises
@@ -36,6 +35,7 @@ public class Exercise implements Writable {
         this.weightLifted = weightLifted;
         this.numSets = numSets;
         this.numReps = numReps;
+        
         EventLog.getInstance().logEvent(new Event("Created a new exercise: " + exerciseName 
                     + " with details: " + weightLifted + "kg, " + numSets 
                     + " sets, " + numReps + " reps, " + muscleType.toString()));
@@ -74,29 +74,22 @@ public class Exercise implements Writable {
     
     // setters
     public void setExerciseName(String exerciseName) {
-        EventLog.getInstance().logEvent(new Event("Updated exercise name to: " + exerciseName));
         this.exerciseName = capitalizationForFirstLetter(exerciseName);
     }
 
     public void setWeightLifted(int weight) {
-        EventLog.getInstance().logEvent(new Event("Updated weight to: " + weight + "kg for exercise: " + exerciseName));
         this.weightLifted = weight;
     }
 
     public void setNumReps(int reps) {
-        EventLog.getInstance().logEvent(new Event("Updated repetitions to: " 
-                            + reps + " for exercise: " + exerciseName));
         this.numReps = reps;
     }
 
     public void setNumSets(int sets) {
-        EventLog.getInstance().logEvent(new Event("Updated sets to " + sets + " for exercise: " + exerciseName));
         this.numSets = sets;
     }
 
     public void setMuscleType(Muscles muscleType) {
-        EventLog.getInstance().logEvent(new Event("Updated muscle type to: " 
-                    + muscleType + " for exercise: " + exerciseName));
         this.muscleType = muscleType;
     }
 
@@ -106,7 +99,7 @@ public class Exercise implements Writable {
         json.put("exercise name", this.exerciseName);
         json.put("muscle Type", this.muscleType);
         json.put("weight", this.weightLifted);
-        json.put("number of sets", this.numSets);
+        json.put("number of Sets", this.numSets);
         json.put("number of Repetitions", this.numReps);
         
         return json;
