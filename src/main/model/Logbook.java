18fb1711a563ed0642cd626c8ffc8132d236753c
Donghwan Lee <dhwanyi@gmail.com>
2025-10-30 17:08:17 -0700

    Delete unnecessary line of the code

--- src/main/model/Logbook.java
+++ src/main/model/Logbook.java
@@ -8,7 +8,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Logbook {
